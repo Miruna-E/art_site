4066075b754e2004674e52a5e8c49d3bc19f5ed4
Miruna_E <dobrica.miruna.e@gmail.com>
2026-07-24 15:00:48 +0300

    Fixed the wheel using trigonometry. Added a function to see how much the wheel rotated and changed the spinning function to also determine the winner. Fixed the wheel's margin and padding so it rotates from the center only. Added an optional container for only the wheel (and commented it). Added a banner for the winner.

--- art_site_function.js
+++ art_site_function.js
@@ -183,6 +183,23 @@ the default action takes place afterwards
 
 
 //wheel of fortune part:
+function getRotationAngle(elementId){
+  var st = window.getComputedStyle(elementId, null);
+  var rotationMatrix = st.getPropertyValue("transform") || "none";
+  if (rotationMatrix != "none") {
+    //rotation matrix:
+    //cos -sin
+    //sin cos
+    var rotationValues = rotationMatrix.split('(')[1].split(')')[0].split(',');
+    var rotationAngle = Math.round(Math.atan2(rotationValues[1],rotationValues[0]) * (180/Math.PI));
+    if(rotationAngle < 0){
+        rotationAngle += 360;
+    }
+    return rotationAngle;
+  }
+  return 0;
+}
+
 let wheelSlices = document.getElementById("wheel_of_fortune").getElementsByTagName("li");
 let sliceRotation = 0;
 for(let slice of wheelSlices){
@@ -190,8 +207,17 @@ for(let slice of wheelSlices){
     sliceRotation += parseFloat(slice.getAttribute("data-percentage"));
 }
 
-//!!!put isMoving in localStorage => wheel won't move when changing page
+function enableClosingBannerButtons(){
+    let closeBannerButtons = document.getElementsByClassName("close_banner_button");
+    for(let button of closeBannerButtons){
+        button.addEventListener("click", () => {
+            button.parentElement.outerHTML = "";
+        });
+    }
+}
+
 let isMoving = true;
+
 document.getElementById("wheel_of_fortune").addEventListener("click", () => {
     let wheel = document.getElementById("wheel_of_fortune");
     if(!isMoving){
@@ -201,5 +227,40 @@ document.getElementById("wheel_of_fortune").addEventListener("click", () => {
         //removing the rotating element class would return to wheel to its original position
         wheel.className = "rotating_element paused_element";
         isMoving = false;
+        //removing the wheel, it's one use only
+        setTimeout(() => {
+            document.getElementById("wheel_figure").outerHTML = "";
+        }, 4000);
     }
+
+    //wheel moves in opposite of clockwise => wheelSlices.length - everything else
+    //slices indexed from 0 => -1
+    //getRotationAngle/36 => 10 slices, each 10% of the circle => 360degrees/10 gives the slice's number (not scalable, sadly)
+    let winnerIndex = wheelSlices.length - 1 - Math.floor(getRotationAngle(document.getElementById("wheel_of_fortune"))/36);
+    let winner = document.getElementById("wheel_of_fortune").getElementsByTagName("li")[winnerIndex];
+
+    //adding the banner, showing the winner:
+    // <div id="wheel_of_fortune_announcement" class="banner center">
+    //     <p>Congratulations! You won <span id="wheel_of_fortune_winner"></span></p>
+    //     <span class="close_banner_button">X</span>
+    // </div> 
+
+    let winnerMessage = document.createElement("p");
+    winnerMessage.innerText = "Congratulations! You won " +  winner.innerText + "!";
+    console.log(winnerMessage.innerText);
+
+    let winnerCloseBannerButton = document.createElement("span");
+    winnerCloseBannerButton.className = "close_banner_button";
+    winnerCloseBannerButton.innerText = "X";
+
+    let winnerAnnouncement = document.createElement("div");
+    winnerAnnouncement.id.add = "wheel_of_fortune_announcement";
+    winnerAnnouncement.classList.add("banner", "center");
+    winnerAnnouncement.appendChild(winnerMessage);
+    winnerAnnouncement.appendChild(winnerCloseBannerButton);
+
+    let winnerContainer = document.getElementById("wheel_of_fortune_winner_container");
+    winnerContainer.appendChild(winnerAnnouncement);
+
+    enableClosingBannerButtons(); //only works if placed at the end of this function
 });
